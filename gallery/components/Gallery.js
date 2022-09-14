6838860f68737aec437ca6135d7cee167c7f20fd
bruh2114 <56392754+bruh2114@users.noreply.github.com>
2022-09-14 12:28:11 +0200

    first commit

---
+++ gallery/components/Gallery.js
@@ -0,0 +1,24 @@
+
+import { Camera, CameraType } from 'expo-camera';
+import React, { useState, useEffect } from 'react';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
+
+function Gallery (props) {
+
+
+  useEffect(()=>{
+    console.log(props.pics)
+  })
+  
+
+  return (
+
+    <View>
+
+    </View>
+  )
+
+}
+
+
+export default Gallery ;
